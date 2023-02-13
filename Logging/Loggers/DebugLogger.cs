719a759dcabb0c7ab5f3dd80a2915e4be21eba28
Will Train <willtrain@virginmedia.com>
2023-02-13 15:08:31 +0000

    Adding Debug logger to write to debug

---
+++ Logging/Loggers/DebugLogger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logging.Loggers
+{
+    public class DebugLogger : ILogger
+    {
+        void ILogger.LogException(LogLevel level, string message, Exception exception)
+            => WriteLogMessage($"{level} {message} \n\t{exception}");
+
+        void ILogger.LogMessage(LogLevel level, string message)
+            => WriteLogMessage($"{level} {message}");
+
+        private void WriteLogMessage(string message)
+        {
+            Debug.WriteLine(message);
+        }
+    }
+}
